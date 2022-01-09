6e3fc54f7a2b06ea1377b90dc3ea62d7f73b0c62
Dolamanee Naik <dolamanee@DESKTOP-AD4KK7A.localdomain>
2022-01-09 22:58:14 +0530

    SignUp feature added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            user_profile = Profile.create(user_id: @user.id)
+            user_profile.save
+            log_in(@user)
+            flash[:success] = "Registered successfully!"
+            redirect_to edit_path
+        else
+            count_flash_msg = 0
+            @user.errors.full_messages.each do |m|
+                flash[count_flash_msg] = m
+                count_flash_msg = count_flash_msg + 1
+            end
+            render 'new'
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root   'home#index'
   get    'edit'    => 'home#edit'
+  get    'signup'  => 'users#new'
+  post   'signup'  => 'users#create'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
