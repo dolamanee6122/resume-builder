b91fd460286e8f92e810311193968dda9c6341e0
Dolamanee Naik <dolamanee@DESKTOP-AD4KK7A.localdomain>
2022-01-12 11:48:51 +0530

    Added sharable link button

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -3,4 +3,10 @@ $(document).ready(function () {
         var image = webkitURL.createObjectURL(this.files[0]);
         $("#image-preview").attr("src", image);
     });
+
+    $("#clipboard-btn").on("click", function () {
+        $("#clipboard-btn").text("Copied!");
+        navigator.clipboard.writeText(this);
+        return false;
+    });
 });
